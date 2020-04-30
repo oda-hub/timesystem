15ba5a36f3b39be0b06957d56fbddab5d2c18e0e
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-30 10:08:39 +0200

    autopep

--- timesystem.py
+++ timesystem.py
@@ -16,47 +16,52 @@ import logging
 import socket
 import traceback
 
-import pilton # type: ignore
+import pilton  # type: ignore
 
-from astropy.table import Table # type: ignore
-from astropy.io import fits # type: ignore
-from astropy.time import Time # type: ignore
-from astropy.coordinates import SkyCoord # type: ignore
+from astropy.table import Table  # type: ignore
+from astropy.io import fits  # type: ignore
+from astropy.time import Time  # type: ignore
+from astropy.coordinates import SkyCoord  # type: ignore
 
 
 def dlog(*a, **aa):
     pass
 
-consul=False
 
-context=socket.gethostname()
+consul = False
+
+context = socket.gethostname()
 
 app = Flask(__name__)
+
 
 class UserException(Exception):
     pass
 
+
 @app.errorhandler(UserException)
 def user_exception(e):
-    return jsonify({'error':str(e)})
+    return jsonify({'error': str(e)})
 
-def converttime_rbp(rbp_var_suffix,informat,intime,outformat):
-    ct=pilton.heatool("converttime")
-    
-    ct['informat']=informat
-    ct['intime']=intime
-    ct['outformat']=outformat
+
+def converttime_rbp(rbp_var_suffix, informat, intime, outformat):
+    ct = pilton.heatool("converttime")
+
+    ct['informat'] = informat
+    ct['intime'] = intime
+    ct['outformat'] = outformat
 
     env = copy.deepcopy(os.environ)
-    env['REP_BASE_PROD'] = env['REP_BASE_PROD_'+rbp_var_suffix] 
+    env['REP_BASE_PROD'] = env['REP_BASE_PROD_'+rbp_var_suffix]
 
     ct.run(env=env)
 
-    return ct.output if hasattr(ct,'output') else None
+    return ct.output if hasattr(ct, 'output') else None
+
 
 def detect_timeformat(t):
     try:
-        t=float(t)
+        t = float(t)
         if t > 20000:
             return 'MJD'
         else:
@@ -68,29 +73,31 @@ def detect_timeformat(t):
         if Time(t).format == "isot":
             return "UTC"
     except Exception as e:
-        raise UserException("unknown time format: %s"%repr(e))
+        raise UserException("unknown time format: %s" % repr(e))
+
 
 @app.route('/api/v1.0/converttime/<string:informat>/<string:intime>/<string:outformat>', methods=['GET'])
-def converttime(informat,intime,outformat):
-    if outformat=="ANY":
-        outformat=""
+def converttime(informat, intime, outformat):
+    if outformat == "ANY":
+        outformat = ""
 
     if informat == "ANY":
         informat = detect_timeformat(intime)
-
 
     problems = []
     output = None
     for rbp_var_suffix in "NRT", "CONS":
         try:
-            output = converttime_rbp(rbp_var_suffix,informat,intime,outformat)
+            output = converttime_rbp(
+                rbp_var_suffix, informat, intime, outformat)
             print("convertime completed")
 
-            r=dict(re.findall("Log_1  : Input Time\(.*?\): .*? Output Time\((.*?)\): (.*?)\n",output,re.S))
+            r = dict(re.findall(
+                "Log_1  : Input Time\(.*?\): .*? Output Time\((.*?)\): (.*?)\n", output, re.S))
 
             print("extracted", r)
 
-            if outformat=="":
+            if outformat == "":
                 return jsonify(r)
             else:
                 if 'is close' in r[outformat]:
@@ -99,21 +106,22 @@ def converttime(informat,intime,outformat):
                 return r[outformat]
 
         except UserException as e:
-            p = {'problem':str(e)}
+            p = {'problem': str(e)}
             print("problem:", p)
-    
+
             problems.append(p)
 
         except Exception as e:
-            p = {'error from converttime':repr(e),'output':output, 'traceback':traceback.format_exc()}
+            p = {'error from converttime': repr(
+                e), 'output': output, 'traceback': traceback.format_exc()}
             print("problem:", p)
-    
+
             problems.append(p)
 
     r = jsonify(problems)
 
-    r.status_code=400
-    dlog(logging.ERROR,"error in converttime "+repr(problems))
+    r.status_code = 400
+    dlog(logging.ERROR, "error in converttime "+repr(problems))
     return r
 
 
@@ -133,14 +141,15 @@ class SCWIDX:
 
         fn = sorted(fns)[-1]
 
-        version = re.search("GNRL-SCWG-GRP-IDX_(.*?).fits.*", os.path.basename(fn)).groups()[0]
+        version = re.search("GNRL-SCWG-GRP-IDX_(.*?).fits.*",
+                            os.path.basename(fn)).groups()[0]
 
         return version, fn
 
     def index(self, rbp, version=None):
         k = (rbp, version)
         if k in self.cache and self.cache[k]['expires_at'] > time.time():
-            print("found cached",k)
+            print("found cached", k)
             return self.cache[k]
 
         if version is None:
@@ -154,20 +163,23 @@ class SCWIDX:
             fn_p = rbp+"/idx/scw/GNRL-SCWG-GRP-IDX_"+version+"*"
             fns = glob.glob(fn_p)
 
-            if len(fns)==0:
+            if len(fns) == 0:
 
                 all_fns = glob.glob(rbp+"/idx/scw/GNRL-SCWG-GRP-IDX_*")
-                versions = sorted([re.search("GNRL-SCWG-GRP-IDX_([0-9]+)\..*?",fn.split("/")[-1]).groups()[0] for fn in all_fns])
+                versions = sorted([re.search(
+                    "GNRL-SCWG-GRP-IDX_([0-9]+)\..*?", fn.split("/")[-1]).groups()[0] for fn in all_fns])
 
                 if int(version) < int(versions[0]):
                     return self.index(rbp, versions[0])
                 else:
                     versions_summary = "%s - %s" % (versions[0], versions[-1])
 
-                    raise UserException("no index with requested version: %s; have: %s"%(version, versions_summary))
+                    raise UserException("no index with requested version: %s; have: %s" % (
+                        version, versions_summary))
 
-            if len(fns)>1:
-                raise UserException("ambigious index with requested version: %s"%version)
+            if len(fns) > 1:
+                raise UserException(
+                    "ambigious index with requested version: %s" % version)
 
             fn = fns[0]
 
@@ -176,34 +188,36 @@ class SCWIDX:
         print("picking", fn, version, expires_at)
 
         r = dict(
-                    table = Table.read(fits.open(fn)[1]), 
-                    table_version = version, 
-                    expires_at = expires_at,
-                )
+            table=Table.read(fits.open(fn)[1]),
+            table_version=version,
+            expires_at=expires_at,
+        )
 
         self.cache[k] = r
 
         return r
-    
+
     def nrt(self, version=None):
         return self.index(os.environ.get('REP_BASE_PROD_NRT'))
-    
+
     def cons(self, version=None):
         return self.index(os.environ.get('REP_BASE_PROD_CONS'))
 
 
 scwidx = SCWIDX()
 
+
 def time2ijd(t):
     try:
-        t=float(t)
+        t = float(t)
 
-        if t < 10000: # IJD
+        if t < 10000:  # IJD
             return t
         else:
-            return t - 51544.0 # MJD
+            return t - 51544.0  # MJD
     except:
         return Time(t).mjd - 51544.0
+
 
 def lastscw_rbp(rbp_var_suffix):
     rbp_var = "REP_BASE_PROD_"+rbp_var_suffix
@@ -214,14 +228,14 @@ def lastscw_rbp(rbp_var_suffix):
     return str(idx['table']['SWID'][-1])
 
 
-def scwlist_rbp(rbp_var_suffix, 
-                index_version: str, 
-                t1: float, 
-                t2: float, 
-                ra: Union[float, None], 
-                dec: Union[float, None], 
-                radius: Union[float, None], 
-                min_good_isgri: Union[float, None], 
+def scwlist_rbp(rbp_var_suffix,
+                index_version: str,
+                t1: float,
+                t2: float,
+                ra: Union[float, None],
+                dec: Union[float, None],
+                radius: Union[float, None],
+                min_good_isgri: Union[float, None],
                 return_columns: Union[str, None],
                 ):
     rbp_var = "REP_BASE_PROD_"+rbp_var_suffix
@@ -234,9 +248,10 @@ def scwlist_rbp(rbp_var_suffix,
     m &= idx['table']['TSTOP'] > t1
 
     if ra is not None and dec is not None and radius is not None:
-        c = SkyCoord(idx['table']['RA_SCX'], idx['table']['DEC_SCX'], unit="deg")
+        c = SkyCoord(idx['table']['RA_SCX'], idx['table']
+                     ['DEC_SCX'], unit="deg")
         m &= c.separation(SkyCoord(ra, dec, unit="deg")).degree < radius
-    
+
     if min_good_isgri is not None:
         m &= idx['table']['TELAPSE'] > min_good_isgri
         m &= idx['table']['IBISMODE'] == 41
@@ -249,21 +264,23 @@ def scwlist_rbp(rbp_var_suffix,
         if c in idx['table'].columns:
             r[c] = idx['table'][c][m].tolist()
         else:
-            m = "undefined column name %s, have: %s"%(c, ", ".join(idx['table'].columns))
+            m = "undefined column name %s, have: %s" % (
+                c, ", ".join(idx['table'].columns))
             print(c, m)
             raise UserException(m)
-  
+
     return r
 
-@app.route('/api/v1.0/scwlist/<string:readiness>/<string:t1>/<string:t2>', methods=['GET'])
-def scwlist(readiness,t1,t2):
-    problems = []
 
+@app.route('/api/v1.0/scwlist/<string:readiness>/<string:t1>/<string:t2>', methods=['GET'])
+def scwlist(readiness, t1, t2):
+    problems = []
 
     ra = request.args.get("ra", default=None, type=float)
     dec = request.args.get("dec", default=None, type=float)
     radius = request.args.get("radius", default=None, type=float)
-    min_good_isgri = request.args.get("min_good_isgri", default=None, type=float)
+    min_good_isgri = request.args.get(
+        "min_good_isgri", default=None, type=float)
     return_columns = request.args.get("return_columns", default=None, type=str)
 
     if readiness.lower() == "any":
@@ -274,12 +291,12 @@ def scwlist(readiness,t1,t2):
         rbp_var_suffixes = ["CONS", ]
     else:
         r = jsonify({'bad request': 'readiness undefined'})
-        r.status_code=400
+        r.status_code = 400
         return r
-    
+
     index_version = request.args.get('index_version', None)
 
-    if len(rbp_var_suffixes)==1:
+    if len(rbp_var_suffixes) == 1:
         if index_version is None:
             rbp_var = "REP_BASE_PROD_"+rbp_var_suffixes[0]
             rbp = os.environ.get(rbp_var)
@@ -288,12 +305,13 @@ def scwlist(readiness,t1,t2):
         else:
             if not re.match("\d+", index_version):
                 r = jsonify({'bad request': "non-conforming index version"})
-                r.status_code=400
+                r.status_code = 400
                 return r
     else:
         if index_version is not None:
-            r = jsonify({'bad request': "index version can only be set with \"cons\" or \"nrt\" source, not \"any\""})
-            r.status_code=400
+            r = jsonify(
+                {'bad request': "index version can only be set with \"cons\" or \"nrt\" source, not \"any\""})
+            r.status_code = 400
             return r
 
     try:
@@ -301,31 +319,30 @@ def scwlist(readiness,t1,t2):
         t2_ijd = time2ijd(t2)
     except ValueError as e:
         r = jsonify({'bad request': 'failed to interpret time: '+repr(e)})
-        r.status_code=400
+        r.status_code = 400
         return r
-        
-        
+
     output = {}
 
     for rbp_var_suffix in rbp_var_suffixes:
         try:
-            for k,v in scwlist_rbp(rbp_var_suffix, index_version, t1_ijd, t2_ijd, ra, dec, radius, min_good_isgri, return_columns=return_columns).items():
+            for k, v in scwlist_rbp(rbp_var_suffix, index_version, t1_ijd, t2_ijd, ra, dec, radius, min_good_isgri, return_columns=return_columns).items():
                 if k not in output:
                     output[k] = []
 
                 output[k] += v
-                
 
         except UserException as e:
-            p = {'problem in '+rbp_var_suffix:str(e)}
+            p = {'problem in '+rbp_var_suffix: str(e)}
             print("problem:", p)
-    
+
             problems.append(p)
 
         except Exception as e:
-            p = {'error from scwlist_rbp':repr(e),'output':output, 'traceback':traceback.format_exc() } # sentry!!
+            p = {'error from scwlist_rbp': repr(
+                e), 'output': output, 'traceback': traceback.format_exc()}  # sentry!!
             print("problem:", p)
-    
+
             problems.append(p)
 
     if problems == []:
@@ -334,13 +351,13 @@ def scwlist(readiness,t1,t2):
 
         if 'debug' in request.args:
             return jsonify(dict(
-                                output=output,
-                                t1_ijd=t1_ijd,
-                                t2_ijd=t2_ijd,
-                                readiness=rbp_var_suffix,
-                                lastscw=lastscw_rbp(rbp_var_suffix),
-                                index_version=index_version,
-                            ))
+                output=output,
+                t1_ijd=t1_ijd,
+                t2_ijd=t2_ijd,
+                readiness=rbp_var_suffix,
+                lastscw=lastscw_rbp(rbp_var_suffix),
+                index_version=index_version,
+            ))
         else:
             if request.args.get('return_index_version', 'no') == "yes":
                 return jsonify(dict(scwlist=output, index_version=index_version))
@@ -349,8 +366,8 @@ def scwlist(readiness,t1,t2):
 
     r = jsonify(problems)
 
-    r.status_code=400
-    dlog(logging.ERROR,"error in converttime "+repr(problems))
+    r.status_code = 400
+    dlog(logging.ERROR, "error in converttime "+repr(problems))
 
     # return index version, last scw
 
@@ -359,29 +376,34 @@ def scwlist(readiness,t1,t2):
     else:
         return r
 
+
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
     r = {}
 
     t = time.time()
-    r['nrev_cons'] = len(glob.glob(os.path.join(os.environ.get("REP_BASE_PROD_CONS"),"scw/*")))
+    r['nrev_cons'] = len(glob.glob(os.path.join(
+        os.environ.get("REP_BASE_PROD_CONS"), "scw/*")))
     r['tspent_cons'] = time.time() - t
 
     t = time.time()
-    r['nrev_nrt'] = len(glob.glob(os.path.join(os.environ.get("REP_BASE_PROD_NRT"),"scw/*")))
+    r['nrev_nrt'] = len(glob.glob(os.path.join(
+        os.environ.get("REP_BASE_PROD_NRT"), "scw/*")))
     r['tspent_nrt'] = time.time() - t
 
     if r['nrev_cons'] > 10 and r['nrev_nrt'] > 10:
-        return jsonify({'status':'OK', **r}), 200
+        return jsonify({'status': 'OK', **r}), 200
     else:
-        return jsonify({'status':'NOK', **r}), 400
+        return jsonify({'status': 'NOK', **r}), 400
 
 
 @app.route('/test', methods=['GET'])
 def test():
     import subprocess
-    c=subprocess.check_output(["python","-m", "pytest", "-sv", "/timesystem/tests"])
+    c = subprocess.check_output(
+        ["python", "-m", "pytest", "-sv", "/timesystem/tests"])
     return c
+
 
 @app.route('/', methods=['GET'])
 @app.route('/poke', methods=['GET'])
@@ -390,7 +412,7 @@ def poke():
 
 
 if __name__ == '__main__':
-    host="0.0.0.0"
-    port=5000
-        
-    app.run(debug=False,port=port,host=host)
+    host = "0.0.0.0"
+    port = 5000
+
+    app.run(debug=False, port=port, host=host)
